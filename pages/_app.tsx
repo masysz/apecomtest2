b489637e6a698d3425147e6b2f3812aecc94715c
masysz <67843831+masysz@users.noreply.github.com>
2024-07-09 20:55:09 +0700

    app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,11 +7,11 @@ import NavBar from "../components/NavBar";
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-// const activeChain = "base";
+// const activeChain = "Base";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain={base}>
+    <ThirdwebProvider activeChain={Base}>
       <ChakraProvider>
         <NavBar/>
         <Component {...pageProps} />
