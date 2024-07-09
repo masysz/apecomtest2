3ada18bdfdd5a4262cd259f03effb68a16adf0c6
masysz <67843831+masysz@users.noreply.github.com>
2024-07-09 19:55:56 +0700

    app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ import NavBar from "../components/NavBar";
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-// const activeChain = "PolygonZkevmTestnet";
+// const activeChain = "base";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
